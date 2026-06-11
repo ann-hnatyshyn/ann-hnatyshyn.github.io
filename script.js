e45ff9d2e621a03871dd0ccb68075b1d42860d82
Ann Hnatyshyn <159579561+ann-hnatyshyn@users.noreply.github.com>
2026-06-11 12:37:36 -0700

    Update script.js
    added PROJECT CASE STUDY PAGES

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
